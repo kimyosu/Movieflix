53a5fe89db115387ff92dc0cee41119c81e0c312
kimyosu <kimyosu@protonmail.com>
2026-06-19 04:16:30 -0300

    Adicionando anotation Builder

--- src/main/java/com/movieflix/entities/Category.java
+++ src/main/java/com/movieflix/entities/Category.java
@@ -7,12 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@Builder
 @Entity
 @Table(name = "category")
 @Data
